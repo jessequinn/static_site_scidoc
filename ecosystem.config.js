cfbf1d4eade9f1bfec1ac39dd0caf600761aa3a6
Jesse Quinn <jesse.quinn@zpesystems.com>
2021-08-14 16:58:23 -0300

    Updated pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'jessequinn',
+      name: 'scidoc',
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
       script: './node_modules/nuxt/bin/nuxt.js',
